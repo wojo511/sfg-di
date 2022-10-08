4c3527f8666245e1a5a794920fe14eb8fac1a3b7
wojtek <wojtek.korys@wp.pl>
2022-10-08 23:12:26 +0200

    app initialization

---
+++ src/test/java/wojtekkorys/springframework/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package wojtekkorys.springframework.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
